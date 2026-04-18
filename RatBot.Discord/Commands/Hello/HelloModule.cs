1fb889316a2140b997fe0089cf8fa28f81a7b4da
= <220043304+rattus-ratgirl@users.noreply.github.com>
2026-04-18 14:47:15 +0100

    oops #2

--- RatBot.Discord/Commands/Hello/HelloModule.cs
+++ RatBot.Discord/Commands/Hello/HelloModule.cs
@@ -1,15 +1,14 @@
 namespace RatBot.Discord.Commands.Hello;
 
-[UsedImplicitly]
+[DefaultMemberPermissions(GuildPermission.SendMessages)]
 public sealed class HelloModule(ILogger logger) : SlashCommandBase
 {
     private readonly ILogger _logger = logger.ForContext<HelloModule>();
 
     [SlashCommand("hello", "Says hello!")]
-    [RequireUserPermission(GuildPermission.SendMessages)]
     public Task HelloAsync()
     {
         _logger.Information("Received hello command from {User}", Context.User.Username);
-        return ReplyAsync($"Hello, {Context.User.Mention}!");
+        return RespondAsync($"Hello, {Context.User.Mention}!");
     }
 }

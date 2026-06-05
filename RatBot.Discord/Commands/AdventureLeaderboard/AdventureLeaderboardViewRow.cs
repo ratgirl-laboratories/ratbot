e5a12f90c9a9bb554e6f8b60b1bcfeb30ebcff8b
= <220043304+rattus-ratgirl@users.noreply.github.com>
2026-06-05 21:49:22 +0100

    refactor adventure leaderboard to use immutable structs and consolidate DTOs

--- RatBot.Discord/Commands/AdventureLeaderboard/AdventureLeaderboardViewRow.cs
+++
@@ -1,8 +0,0 @@
-namespace RatBot.Discord.Commands.AdventureLeaderboard;
-
-public sealed record AdventureLeaderboardViewRow(
-    int Rank,
-    int Score,
-    int MaxScore,
-    string Progress,
-    string DisplayName);
